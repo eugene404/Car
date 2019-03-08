b0d2c5d0c395e49dd7711913475354b906360ede
Eugene <eugenesny@icloud.com>
2019-03-08 16:52:20 +0200

    Add project files.

---
+++ Car/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Car
+{
+
+    class Car
+    {
+
+        int carName;
+        int wheels = 4;
+
+        public Car()
+        {
+            
+        }
+
+                public double gearRatio(int finalDrive)
+            {
+                return finalDrive * 4.2 ;
+            }
+
+        static void Main(string[] args)
+        {
+            Car bmw = new Car();
+
+            Console.WriteLine();
+        }
+    }
+}
